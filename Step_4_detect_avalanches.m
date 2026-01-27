4f569d395575e9913a70773c65ce3f04550d6ad8
pgill24 <pgill24@student.ubc.ca>
2026-01-26 21:28:29 -0800

    Minor changed: made changes to Step_4_avalanches (noted where I changed it from original code for single subject)

--- Step_4_detect_avalanches.m
+++ Step_4_detect_avalanches.m
@@ -3,17 +3,19 @@ clc;
 clear;
 close all
 addpath('Functions')
+
 %% detect the avalanches of HY48-ROIsignals
 mkdir(fullfile('step_4_avalanches', 'ROI_level', 'HY48'));
 clc;
 clear;
 close all;
 timebinsize = 1;
-ROI_number = 48;
+ROI_number = 48;   %they have 96
 time_length = 1200;
-for SUB = 1
+for SUB = 1.      % changed from 1:295 as we only have 1 subject right now!
     subj_ID = ['sub_', num2str(SUB, '%03d')];
-    load(fullfile('step_3_events', 'ROI_level', 'HY48', 'sub_001.mat'), 'peakevents');
+    %load(fullfile('step_3_events', 'ROI_level', 'HY96', [subj_ID, '.mat']), 'peakevents');
+    load(fullfile('step_3_events', 'ROI_level', 'HY48', 'sub_001.mat'), 'peakevents'); % changed as only 1 subject!
     for THR = 1: 25
         raster = peakevents.ithr(THR).raster;
         avalanches_HY48(SUB).threshold(THR).threshold = THR/10;
